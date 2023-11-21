f5d1addee5320343c1e7b0f5bcadeae653ffc284
im-luka <lux.9833@gmail.com>
2023-11-21 18:47:29 +0100

    ⚡️ feat: theme setup - components

--- app/_components/providers/theme/index.tsx
+++ app/_components/providers/theme/index.tsx
@@ -8,6 +8,7 @@ import { Notifications } from "@mantine/notifications";
 import { colors } from "./colors";
 import { typography } from "./typography";
 import { spacing } from "./spacing";
+import { components } from "./components";
 
 type Props = {
   children: ReactNode;
@@ -17,6 +18,7 @@ const theme = createTheme({
   ...colors,
   ...typography,
   spacing,
+  components,
 });
 
 export const ThemeProvider: FC<Props> = ({ children }) => {
