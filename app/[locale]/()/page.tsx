3b31c606be9cb692d0104bdc9418ad0b36e2d3c9
im-luka <lux.9833@gmail.com>
2023-11-22 21:59:02 +0100

    ⚡️ feat: session provider

--- app/[locale]/()/page.tsx
+++ app/[locale]/()/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { useTranslations } from "next-intl";
-import { Box, Title } from "@mantine/core";
+import { signOut, useSession } from "next-auth/react";
+import { Box, Button, Title } from "@mantine/core";
 
 export default function Home() {
   const t = useTranslations();
+  const session = useSession();
+  console.log(session);
 
   return (
     <Box bg="primary">
       <Title>{t("appName")}</Title>
+      <Button onClick={() => signOut()}>Log Out</Button>
     </Box>
   );
 }
