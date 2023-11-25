11672c2fecc1c127a789af280da08f912f54fee7
im-luka <lux.9833@gmail.com>
2023-11-25 22:09:39 +0100

    ⚡️ feat: add project from sidebar

--- app/_components/projects/projects-sidebar.tsx
+++ app/_components/projects/projects-sidebar.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import { FC } from "react";
-import { Divider, Stack, Title } from "@mantine/core";
+import { ActionIcon, Divider, Group, Stack, Title } from "@mantine/core";
 import { Project } from "@/types/project";
 import { ProjectSidebarItem } from "./project-sidebar-item";
 import styles from "@/styles/components/projects-sidebar.module.scss";
+import { User } from "@prisma/client";
+import { useDisclosure } from "@mantine/hooks";
+import { ProjectAddModal } from "../modals/project-add-modal";
+import { IconPlus } from "@tabler/icons-react";
 
 type Props = {
   title: string;
   projects: Project[];
+  users: User[];
 };
 
 export const ProjectsSidebar: FC<Props> = (props) => {
-  const { title, projects } = useProjectsSidebar(props);
+  const { title, projects, users, isOpen, open, close } =
+    useProjectsSidebar(props);
 
   const renderProject = (project: Project) => (
     <ProjectSidebarItem key={project.id} project={project} />
@@ -18,15 +26,21 @@ export const ProjectsSidebar: FC<Props> = (props) => {
 
   return (
     <Stack h="100%" pr="lg" py="md" gap="xl" className={styles.projectsSidebar}>
-      <Title order={6} ta="center">
-        {title}
-      </Title>
+      <Group gap="xl" justify="space-between">
+        <Title order={6}>{title}</Title>
+        <ActionIcon size="xs" variant="subtle" onClick={open}>
+          <IconPlus />
+        </ActionIcon>
+      </Group>
       <Divider />
       <Stack gap="xs">{projects.map(renderProject)}</Stack>
+      <ProjectAddModal isOpen={isOpen} onClose={close} users={users} />
     </Stack>
   );
 };
 
-function useProjectsSidebar({ title, projects }: Props) {
-  return { title, projects };
+function useProjectsSidebar({ title, projects, users }: Props) {
+  const [isOpen, { open, close }] = useDisclosure(false);
+
+  return { title, projects, users, isOpen, open, close };
 }
